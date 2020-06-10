36400909a5be2fa5d90b56486954687648d30174
Vic Chimenti <chimenti@seattleu.edu>
2020-06-10 09:56:47 -0700

    rm nl

--- filter-type.js
+++ filter-type.js
@@ -13,18 +13,18 @@ try {
 
     if (fieldZone == 'ZoneA') {
 
-        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" >\n  <div id="filterField" class="standardContent">');
-        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-        document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div></div>\n</div>\n');
-        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>\n');
+        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneA" data-position-selected="ZoneA" ><div id="filterField" class="standardContent">');
+        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
+        document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
+        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
         document.write('</div>');
 
     } else if (fieldZone == 'ZoneB') {
 
-        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneB" data-position-selected="ZoneB" >\n  <div id="filterField" class="standardContent">');
-        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>\n');
-        document.write('<div>\n ' + fieldFieldCode + '<div class="clearfix"></div></div>\n</div>\n');
-        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>\n');
+        document.write('<div id="filterFieldWrapper" class="contentItem" data-posistion-default="ZoneB" data-position-selected="ZoneB" ><div id="filterField" class="standardContent">');
+        document.write('<div class="filterFieldInstructions">' + fieldInstructions + '</div>');
+        document.write('<div>' + fieldFieldCode + '<div class="clearfix"></div></div></div>');
+        document.write('<div class="noResultsToShow standardContent hideResultsMessage">' + message + '</div>');
         document.write('</div>');
 
     }
