c9a59fdc95c2398bc8337cd80d3dc529d853853c
Victor Chimenti <chimenti@seattleu.edu>
2020-06-25 10:56:20 -0700

    update link to base

--- newOrganizer.js
+++ newOrganizer.js
@@ -12,7 +12,7 @@ function readMedia(mediaID) {
 
 try {
 	// Import Organizer base from media library
-	var base = readMedia(163514);
+	var base = readMedia(1607015);
 	eval(String(base));
 	// Set content wrapper, if any
 	var header, midder, footer;
